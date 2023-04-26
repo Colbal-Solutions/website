ce10a2f389fa784e1e9a48f1a227543d23fa5ed1
Charles Panter <squidy.p@gmail.com>
2023-04-26 09:43:14 -0700

    place holder

--- static/js/cookie-banner.js
+++ static/js/cookie-banner.js
@@ -1,9 +1,1 @@
-<script type="text/javascript" id="cookieinfo"
-	src="//cookieinfoscript.com/js/cookieinfo.min.js"
-	data-bg="#645862"
-	data-fg="#FFFFFF"
-	data-link="#F1D600"
-	data-cookie="CookieInfoScript"
-	data-text-align="left"
-       data-close-text="Got it!">
-</script>
+//cookie warning banner goes here... when we need it.
